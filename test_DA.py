387e5561bde2f01cd328012d33ff224e95508683
flimdejong <flimdejong@proton.me>
2026-04-29 11:35:26 +0200

    Testing code for both YOLO26 and DA2

---
+++ test_DA.py
@@ -0,0 +1,14 @@
+from transformers import pipeline
+from PIL import Image
+import requests
+
+# load pipe
+pipe = pipeline(task="depth-estimation", model="depth-anything/Depth-Anything-V2-Small-hf")
+
+# load image
+url = 'http://images.cocodataset.org/val2017/000000039769.jpg'
+image = Image.open(requests.get(url, stream=True).raw)
+
+# inference
+depth = pipe(image)["depth"]
+depth.show() 
